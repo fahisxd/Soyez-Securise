241d9f7cedea7505e6c32193e8c004b07c080401
Fahis <fahisshehandim@gmail.com>
2026-06-13 22:39:20 +0530

    Update main.py

--- soyezsecurise/soyezsecurise-backend/main.py
+++ soyezsecurise/soyezsecurise-backend/main.py
@@ -328,17 +328,17 @@ app.add_middleware(
 
 )
 
-# @app.middleware("http")
-# async def security_headers(request, call_next):
-#     response = await call_next(request)
+@app.middleware("http")
+async def security_headers(request, call_next):
+    response = await call_next(request)
     
-#     response.headers["X-Content-Type-Options"] = "nosniff"
-#     response.headers["X-Frame-Options"] = "DENY"
-#     response.headers["X-XSS-Protection"] = "1; mode=block"
-#     response.headers["Referrer-Policy"] = "strict-origin-when-cross-origin"
-#     response.headers["Content-Security-Policy"] = "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; object-src 'none'; frame-ancestors 'none';"
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    response.headers["X-Frame-Options"] = "DENY"
+    response.headers["X-XSS-Protection"] = "1; mode=block"
+    response.headers["Referrer-Policy"] = "strict-origin-when-cross-origin"
+    response.headers["Content-Security-Policy"] = "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; object-src 'none'; frame-ancestors 'none';"
     
-#     return response
+    return response
 
 
 #--------------------------------------------APIs--------------------------------------------------#
